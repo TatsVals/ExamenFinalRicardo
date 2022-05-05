b152772f41ab0ac351930a2caeeb04db8121a702
CARLOS RICARDO VALLADARES <cvalladaresb0346@uvirtual.ac.cr>
2022-05-05 07:10:51 -0600

    Add project files.

---
+++ WebAppLab/App_Start/ContainerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BD;
+using WBL;
+namespace WebAppLab
+{
+    public static class ContainerExtensions
+    {
+
+        public static IServiceCollection AddDIContainer(this IServiceCollection services)
+        {
+
+            services.AddSingleton<IDataAccess,DataAccess>();
+
+            services.AddTransient<IMarcaVehiculoService, MarcaVehiculoService>();
+            services.AddTransient<IVehiculoService, VehiculoService>();
+            services.AddTransient<IAgenciaService, AgenciaService>();
+            services.AddTransient<ICatalogoProvinciaService, CatalogoProvinciaService>();
+            services.AddTransient<ICatalogoCantonService, CatalogoCantonService>();
+            services.AddTransient<ICatalogoDistritoService, CatalogoDistritoService>();
+            services.AddTransient<IClientesService, ClientesService>();
+            services.AddTransient<IUsuariosServices, UsuariosServices>();
+            services.AddTransient<IAlquilerService, AlquilerService>();
+
+            return services;
+        }
+    }
+}
